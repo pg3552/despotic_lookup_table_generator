04d8a329445642ea71d591d6166299d52f0b70d5
desika <none@none>
2017-06-21 15:39:18 -0400

    better comments in example

--- example.py
+++ example.py
@@ -14,3 +14,12 @@ temp_wco_grid,temp_wci_grid,temp_wcii_grid,temp_h2_abu_grid,temp_hi_abu_grid = g
 #temp_wco_grid will return a 10 x n_dim array where the 10 dimension
 #is the first 10 levels of CO, and the n_dim are the number of
 #dimensions of your (e.g.) column_points array.  the units are K-km/s
+
+#wci and wcii are the same but for CI and [CII]
+
+#h2_abu_grid and hi_abu_grid are the H2 and HI abundances such that:
+
+'''
+In [7]: temp_h2_abu_grid*2+temp_hi_abu_grid
+Out[7]: array([ 0.99997152,  0.99984029,  0.99998034])
+'''
