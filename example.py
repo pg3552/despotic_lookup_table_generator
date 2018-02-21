ca0775fe54d3486baca16de521b2a18cf7796226
desika <none@none>
2018-02-21 14:20:32 -0500

    small changes cleaning up the example

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
-# standard version without redshift information
+# standard version without redshift information and abundances
 
 from lookup_table_reader import *
 import numpy as np
-
-lookup_npzfilename = "/home/desika.narayanan/despotic_lookup_table_generator/high_res.npz"#_fixed_emission_line_width.npz"
+lookup_npzfilename = "/home/desika.narayanan/despotic_lookup_table_generator/high_res.npz"
 
 column_points = np.asarray([25,50,100]) #Msun/pc^2
 nh_points = np.asarray([50,100,1000]) #cm^-3
@@ -14,24 +13,21 @@ temp_wco_grid,temp_wci_grid,temp_wcii_grid,temp_h2_abu_grid,temp_hi_abu_grid = g
 
 #temp_wco_grid will return a 10 x n_dim array where the 10 dimension
 #is the first 10 levels of CO, and the n_dim are the number of
-#dimensions of your (e.g.) column_points array.  the units are K-km/s
+#dimensions of your (e.g.) column_points array.  the units are K-km/s.
+#If you set intensity=False then the output will be erg/s/H
 
 #wci and wcii are the same but for CI and [CII]
 
 #h2_abu_grid and hi_abu_grid are the H2 and HI abundances such that:
 
-'''
-In [7]: temp_h2_abu_grid*2+temp_hi_abu_grid
-Out[7]: array([ 0.99997152,  0.99984029,  0.99998034])
-'''
 
-# version with redshift information
+# version with redshift information and abundances
 from lookup_table_reader_z import *
 import numpy as np
 
 lookup_npzfilename = "/home/desika.narayanan/despotic_lookup_table_generator/high_res_abu_z.npz"#_fixed_emission_line_width.npz"
 
-redshift_points = np.asarray([0.,1.,1.]) #Msun/pc^2
+redshift_points = np.asarray([0.,1.,1.]) 
 column_points = np.asarray([25,50,100]) #Msun/pc^2
 nh_points = np.asarray([50,100,1000]) #cm^-3
 sfr_points = np.asarray([1,10,20]) #msun/yr
